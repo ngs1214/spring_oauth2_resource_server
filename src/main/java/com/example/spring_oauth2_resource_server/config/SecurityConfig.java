4ae5a66cd951ec9c5d35600654c3292f0d6c1a12
sean <ngs1214@gmail.com>
2024-11-01 08:51:47 +0900

    리소스 응답처리

--- src/main/java/com/example/spring_oauth2_resource_server/config/SecurityConfig.java
+++ src/main/java/com/example/spring_oauth2_resource_server/config/SecurityConfig.java
@@ -2,18 +2,40 @@ package com.example.spring_oauth2_resource_server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    JwtDecoder jwtDecoder() {
+    public JwtDecoder jwtDecoder() {
         //인가서버 주소
         //자동으로 공개키를 찾아 디코더 진행
         return JwtDecoders.fromIssuerLocation("http://localhost:9000");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .oauth2ResourceServer((resource) -> resource.jwt(Customizer.withDefaults()));
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/me").hasAuthority("SCOPE_profile")
+                        .requestMatchers("/admin").hasAnyAuthority("SCOPE_admin", "ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                );
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
     }
 }
